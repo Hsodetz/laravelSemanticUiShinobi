e2edc0a3a61a7efed9a1b0fa4fbaed8b08e4f01c
Helmuth Sodetz <sodehl2001@gmail.com>
2018-05-04 17:53:22 -0500

    Se agragaron los middleware en el kernel.php y se crearon las rutas con sus middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -59,5 +59,9 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+
+        // Agregamos los middleware para shinobi.
+        'role'          => \Caffeinated\Shinobi\Middleware\UserHasRole::class,
+        'permission'    => \Caffeinated\Shinobi\Middleware\UserHasPermission::class,
     ];
 }
